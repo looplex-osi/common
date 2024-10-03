c8cdaf0dbb506ca5a89cb41ca743554144c7ec25
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2024-10-03 09:29:51 -0300

    feat: implement `debounce`

---
+++ src/lib/debounce.mjs
@@ -0,0 +1,33 @@
+/**
+ * Creates a debounced version of the provided function that delays its execution
+ * until after `delay` milliseconds have elapsed since the last time it was invoked.
+ * Useful for reducing the rate at which a function is called.
+ *
+ * @param {Function} fun - The function to debounce.
+ * @param {number} delay - The number of milliseconds to delay execution.
+ * @returns {Function} A debounced version of the provided function.
+ *
+ * @example
+ * // Debounce a resize event handler
+ * window.addEventListener('resize', debounce(() => {
+ *   console.log('Resize event handler called')
+ * }, 300))
+ *
+ * // Debounce a search input handler
+ * const debouncedSearch = debounce((query) => {
+ *   performSearch(query)
+ * }, 500)
+ *
+ * inputElement.addEventListener('input', (e) => {
+ *   debouncedSearch(e.target.value)
+ * })
+ */
+export function debounce (fun, delay) {
+  let timeoutId
+  return function (...args) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fun.apply(this, args), delay)
+  }
+}
+
+export default debounce
